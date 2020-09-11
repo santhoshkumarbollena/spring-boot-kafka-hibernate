163c6ef47ea737beca35bf9acfee65518faeba43
santhosh kumar bollena <35809276+santhoshkumarbollena@users.noreply.github.com>
2020-09-11 01:02:53 -0500

    Aded response code

--- src/main/java/com/example/Kafka/Controller/KafkaHibController.java
+++ src/main/java/com/example/Kafka/Controller/KafkaHibController.java
@@ -3,10 +3,12 @@ package com.example.Kafka.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Kafka.Model.User;
@@ -21,6 +23,7 @@ public class KafkaHibController {
 	KafkaTemplate<String,String> kt;
 
 	  @GetMapping("/users")
+	  @ResponseStatus(code=HttpStatus.BAD_REQUEST)
 	  List<User> all() {
 		 List<User> users= repository.findAll();
 	    kt.send("bearcat-messages",users.toString());
